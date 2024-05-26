d3756943eb09a9a514ba965c4322d4bd38e12f04
FloridaSuka <155165730+FloridaSuka@users.noreply.github.com>
2024-05-26 18:15:25 +0200

    Update LeetCode.cpp

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -538,6 +538,26 @@ ListNode* removeElements(ListNode* head, int val) {
 //O(nlogn)
 //Kompleksiteti hapsinor
 //O(n)
+
+//5)Two Sum
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> map; // Harta për ruajtjen e numrave dhe indekseve të tyre
+        vector<int> result;
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            // Kontrollon nëse ekziston komplementi në hartë
+            if (map.find(complement) != map.end()) {
+                result.push_back(map[complement]);
+                result.push_back(i);
+                return result;
+            }
+            // Ruaj indeksin e çdo elementi në hartë
+            map[nums[i]] = i;
+        }
+        return result; // Kthe indekset e dy numrave që shtohen në target
+    }
+//Kompleksiteti khore dhe hapsinor O(n)
+
  };
 
 void printInOrder(TreeNode* node) {
